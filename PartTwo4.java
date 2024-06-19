d09971262cf4d573dc91d15234712119838662de
unknown <hastycodeandreh@gmail.com>
2024-06-19 11:17:09 +0300

    updated

--- PartTwo4.java
+++ PartTwo4.java
@@ -6,21 +6,27 @@ public class PartTwo4 {
         System.out.print("Specify line height: ");
         int height = Integer.valueOf(scanner.nextLine());
 
-        for(int i = 1; i <= height; i++) {
-            printSpaces(height - i);
-            printNumber(i);
+        for (int i = 1; i <= height; i++) {
+            int number = (2 * i) - 1;
+            int space = height - i;
+
+            if (number % 2 == 1) {
+                printSpaces(space);
+                printNumber(i);
+            }
+
             System.out.println();
         }
     }
 
     public static void printSpaces(int number) {
-        for(int i = 0; i < number; i++ ) {
+        for (int i = 0; i < number; i++) {
             System.out.print(" ");
         }
     }
 
     public static void printNumber(int number) {
-        for(int i = 1; i <= number; i++) {
+        for (int i = 1; i <= number; i++) {
             System.out.print(i);
         }
     }
