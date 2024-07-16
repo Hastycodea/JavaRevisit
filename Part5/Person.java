0955df2d7dffcc6264c5ced87762cc44ea7e366a
hastycodea <hastycodeandreh@gmail.com>
2024-07-16 21:14:20 +0300

    Lists+Classes

---
+++ Part5/Person.java
@@ -0,0 +1,20 @@
+package Part5;
+
+public class Person {
+    private String name;
+    private String id;
+    private String gender;
+    private String county;
+
+    public Person(String name, String id, String gender, String county) {
+        this.name = name; 
+        this.id = id;
+        this.gender = gender;
+        this.county = county;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{ name=" + this.name; 
+    }
+}
