5489714311ab2326d0ebf315ca545affe9337081
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-04 07:02:33 +0300

    Updated commands

--- Part7/Program.java
+++ Part7/Program.java
@@ -37,24 +37,62 @@ public class Program {
         }
 
         System.out.println("Commands: ");
-        System.out.println("list - lists the movies \nstop - stops the program\n");
+        System.out.println("list - lists the movies");
+        System.out.println("stop - stops the program");
+        System.out.println("find title - searches movies by title");
+        System.out.println("find release year - searches movies by release year");
+        System.out.println("find genre - searches movies by genre");
         Scanner scan = new Scanner(System.in);
+        System.out.println();
 
         while (true) {
             System.out.print("Enter command: ");
             String command = scan.nextLine(); 
 
-            if (command.equals("stop")) {
+            if (command.equalsIgnoreCase("stop")) {
                 break;
-            } else if (command.equals("list")) {
+            } else if (command.equalsIgnoreCase("list")) {
                 System.out.println("Movies:");
                 for(Movies movie : movies) {
                     System.out.println(movie.toString());
+                }
+            } else if (command.equalsIgnoreCase("find title")) {
+                Scanner scanner2 = new Scanner(System.in);
+                System.out.print("Searched word: ");
+                String title = scanner2.nextLine();
+
+                for (Movies movie : movies) {
+                    if (movie.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                        System.out.println(movie.toString());
+                    }
+                }
+
+            } else if (command.equalsIgnoreCase("find release year")) {
+                Scanner scanner3 = new Scanner(System.in);
+                System.out.print("Earliest release year: ");
+                int year = Integer.parseInt(scanner3.nextLine());
+                for(Movies movie : movies) {
+                    if (movie.getReleaseYear() >= year) {
+                        System.out.println(movie.toString());
+                    }
+                }
+            } else if (command.equalsIgnoreCase("find genre")) {
+                Scanner scanner4 = new Scanner(System.in);
+                System.out.print("Genre: ");
+                String genreInput = scanner4.nextLine();
+                for(Movies movie : movies) {
+                    for(String genre : movie.getGenre()) {
+                        if (genre.toLowerCase().contains(genreInput.toLowerCase())) {
+                            System.out.println(movie.toString());
+                        }
+                    }
                 }
             }
             System.out.println();
         }
 
     }
+
+    
     
 }
